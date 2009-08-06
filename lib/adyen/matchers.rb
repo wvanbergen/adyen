89672d69fb9d4b928ea4611b934a9d15917ecfd1
Willem van Bergen <willem@vanbergen.org>
2009-08-06 18:02:22 +0200

    Improved README

--- lib/adyen/matchers.rb
+++ lib/adyen/matchers.rb
@@ -25,17 +25,21 @@ module Adyen
       end
 
       def self.document(subject)
-        case subject
-        when 
-          
+        if String === subject
+          XML::HTMLParser.string(subject).parse
+        elsif document.respond_to?(:body)
+          XML::HTMLParser.string(subject.body).parse
+        elsif XML::Node === subject
+          subject
+        elsif XML::Document === subject
+          subject
         else
-          return 
+          raise "Cannot handle this XML input type"
         end
       end
 
       def self.check(subject, checks = {})
-        document = document(subject)
-        XML::HTMLParser.string(document).parse.find_first(build_xpath_query(@checks))        
+        document(subject).find_first(build_xpath_query(checks))
       end      
     end
     
@@ -44,31 +48,9 @@ module Adyen
       def initialize(checks)
         @checks = checks
       end
-      
-      def build_xpath_query(checks)
-        xpath_query =  "//form[@action='#{AdyenPaymentIntegrator::Form.url}']" # Adyen form 
-
-        recurring =  checks.delete(:recurring)
-        unless recurring.nil?
-          if recurring
-            xpath_query << "[descendant::input[@type='hidden'][@name='recurringContract']]"          
-          else
-            xpath_query << "[not(descendant::input[@type='hidden'][@name='recurringContract'])]"
-          end
-        end
-
-        checks.each do |key, value|
-          condition  = "descendant::input[@type='hidden'][@name='#{key.to_s.camelize(:lower)}']"
-          condition << "[@value='#{value}']" unless value == :anything
-          xpath_query << "[#{condition}]"
-        end 
-        
-        return xpath_query       
-      end
     
       def matches?(document)
-        document = document.body if document.respond_to?(:body)
-        XML::HTMLParser.string(document).parse.find_first(build_xpath_query(@checks))
+        Adyen::Matchers::XPathCheck.check(document, @checks)
       end
   
       def description
@@ -97,6 +79,23 @@ module Adyen
     def have_adyen_single_payment_form(checks = {})
       recurring_checks = { :recurring => false }
       have_adyen_payment_form(recurring_checks.merge(checks))
-    end       
+    end
+    
+    
+    def assert_adyen_payment_form(subject, checks = {})
+      default_checks = {:merchant_sig => :anything, :order_data => :anything }
+      XPathCheck.check(subject, default_checks.merge(checks))
+    end
+    
+    def assert_adyen_recurring_payment_form(subject, recurring_checks = {})
+      recurring_checks = { :recurring => true, :shopper_email => :anything, :shopper_reference => :anything }
+      assert_adyen_payment_form(subject, recurring_checks.merge(checks))
+    end           
+
+    def assert_adyen_single_payment_form(subject, recurring_checks = {})
+      recurring_checks = { :recurring => false }
+      assert_adyen_payment_form(subject, recurring_checks.merge(checks))
+    end    
+    
   end
 end
