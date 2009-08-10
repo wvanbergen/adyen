367fd6f7475a93a179bc83346954872077b8c2d9
Michel Barbosa <me@cicaboo.com>
2009-08-10 12:46:56 +0200

    moved encoding and formatting logic to their respective modules, changed specs accordingly

---
+++ lib/adyen/encoding.rb
@@ -0,0 +1,21 @@
+require 'base64'
+require 'openssl'
+require 'stringio'
+require 'zlib'
+
+module Adyen
+  module Encoding
+    def self.hmac_base64(hmac_key, message)
+      digest = OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), hmac_key, message)
+      Base64.encode64(digest).strip
+    end
+
+    def self.gzip_base64(message)
+      sio = StringIO.new
+      gz  = Zlib::GzipWriter.new(sio)
+      gz.write(message)
+      gz.close
+      Base64.encode64(sio.string).gsub("\n", "")
+    end
+  end
+end
---
+++ lib/adyen/formatter.rb
@@ -0,0 +1,37 @@
+module Adyen
+  module Formatter
+    module DateTime
+      # Returns a valid Adyen string representation for a date
+      def self.fmt_date(date)
+        case date
+        when Date, DateTime, Time
+          date.strftime('%Y-%m-%d')
+        else
+          raise "Invalid date notation: #{date.inspect}!" unless /^\d{4}-\d{2}-\d{2}$/ =~ date
+          date
+        end
+      end
+
+      # Returns a valid Adyen string representation for a timestamp
+      def self.fmt_time(time)
+        case time
+        when Date, DateTime, Time
+          time.strftime('%Y-%m-%dT%H:%M:%SZ')
+        else
+          raise "Invalid timestamp notation: #{time.inspect}!" unless /^\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}Z$/ =~ time
+          time
+        end
+      end
+    end
+
+    module Price
+      def self.in_cents(price)
+        ((price * 100).round).to_i
+      end
+      
+      def self.from_cents(price)
+        BigDecimal.new(price.to_s) / 100
+      end
+    end
+  end
+end
