e98dfcaf333dec143568ec307f7b095ae5771ff0
Michel Barbosa <me@cicaboo.com>
2009-08-10 12:46:33 +0200

    moved encoding and formatting logic to their respective modules, changed specs accordingly

--- lib/adyen.rb
+++ lib/adyen.rb
@@ -1,65 +1,13 @@
-require 'base64'
-require 'openssl'
-require 'stringio'
-require 'zlib'
-
 module Adyen
   LIVE_RAILS_ENVIRONMENTS = ['production']  
 
   def self.autodetect_environment
     (defined?(RAILS_ENV) && Adyen::LIVE_RAILS_ENVIRONMENTS.include?(RAILS_ENV)) ? 'live' : 'test'
   end
-  
-  # Returns a valid string representation for a date to use in Adyen communications
-  def self.date(date)
-    case date
-    when Date, DateTime, Time
-      date.strftime('%Y-%m-%d')
-    else
-      raise "Invalid date notation: #{date.inspect}!" unless /^\d{4}-\d{2}-\d{2}$/ =~ date
-      date
-    end
-  end
-  
-  # Returns a valid string representation for a timestamp to use in Adyen communications
-  def self.time(time)
-    case time
-    when Date, DateTime, Time
-      time.strftime('%Y-%m-%dT%H:%M:%SZ')
-    else
-      raise "Invalid timestamp notation: #{time.inspect}!" unless /^\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}Z$/ =~ time
-      time
-    end
-  end
-  
-  module Price
-    def self.in_cents(price)
-      ((price * 100).round).to_i.to_s
-    end
-
-    def self.from_cents(price)
-      BigDecimal.new(price.to_s) / 100
-    end
-  end
-
-  module Encoding
-
-    def self.hmac_base64(hmac_key, message)
-      digest = OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), hmac_key, message)
-      Base64.encode64(digest).strip
-    end
-
-    def self.gzip_base64(message)
-      sio = StringIO.new
-      gz  = Zlib::GzipWriter.new(sio)
-      gz.write(message)
-      gz.close
-      Base64.encode64(sio.string).gsub("\n", "")
-    end
-  end
-
 end
 
+require 'adyen/encoding'
 require 'adyen/form'
+require 'adyen/formatter'
 require 'adyen/notification'
 require 'adyen/soap'
--- spec/adyen_spec.rb
+++ spec/adyen_spec.rb
@@ -1,42 +1,53 @@
 require "#{File.dirname(__FILE__)}/spec_helper.rb"
 
 describe Adyen do
+  describe Adyen::Encoding do
+    it "should a hmac_base64 correcly" do
+      encoded_str = Adyen::Encoding.hmac_base64('bla', 'bla')
+      encoded_str.should_not be_blank
+      encoded_str.size.should == 28
+    end    
+    
+    it "should gzip_base64 correcly" do
+      encoded_str = Adyen::Encoding.gzip_base64('bla')
+      encoded_str.should_not be_blank
+      encoded_str.size.should == 32
+    end
+  end
 
-  describe 'dates en times' do
+  describe Adyen::Formatter::DateTime do
     it "should accept dates" do
-      Adyen.date(Date.today).should match(/^\d{4}-\d{2}-\d{2}$/)
+      Adyen::Formatter::DateTime.fmt_date(Date.today).should match(/^\d{4}-\d{2}-\d{2}$/)
     end
     
     it "should accept times" do
-      Adyen.time(Time.now).should match(/^\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}Z$/)
+      Adyen::Formatter::DateTime.fmt_time(Time.now).should match(/^\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}Z$/)
     end
     
     it "should accept valid time strings" do
-      Adyen.time('2009-01-01T11:11:11Z').should match(/^\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}Z$/)
+      Adyen::Formatter::DateTime.fmt_time('2009-01-01T11:11:11Z').should match(/^\d{4}-\d{2}-\d{2}T\d{2}\:\d{2}\:\d{2}Z$/)
     end    
     
     it "should accept valid time strings" do    
-      Adyen.date('2009-01-01').should match(/^\d{4}-\d{2}-\d{2}$/)
+      Adyen::Formatter::DateTime.fmt_date('2009-01-01').should match(/^\d{4}-\d{2}-\d{2}$/)
     end
 
     it "should raise on an invalid time string" do
-      lambda { Adyen.time('2009-01-01 11:11:11') }.should raise_error
+      lambda { Adyen::Formatter::DateTime.fmt_time('2009-01-01 11:11:11') }.should raise_error
     end    
 
     it "should raise on an invalid date string" do
-      lambda { Adyen.date('2009-1-1') }.should raise_error
+      lambda { Adyen::Formatter::DateTime.fmt_date('2009-1-1') }.should raise_error
     end    
   end
 
-  describe Adyen::Price do
-
-    it "should return a string with digits only when converting to cents" do
-      Adyen::Price.in_cents(33.76).should match(/^\-?\d+/)
+  describe Adyen::Formatter::Price do
+    it "should return a Fixnum with digits only when converting to cents" do
+      Adyen::Formatter::Price.in_cents(33.76).should be_kind_of(Fixnum)
     end
 
     it "should return a BigDecimal when converting from cents" do
-      Adyen::Price.from_cents(1234).should be_kind_of(BigDecimal)
+      Adyen::Formatter::Price.from_cents(1234).should be_kind_of(BigDecimal)
     end
   end
 end
-
