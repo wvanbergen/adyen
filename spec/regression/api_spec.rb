6bb7ab23395d5f7082a4361c54234a55d712d7cd
Eloy Duran <eloy.de.enige@gmail.com>
2010-09-27 13:01:44 +0200

    Add regression spec for authorise_payment_request_body.

--- spec/regression/api_spec.rb
+++ spec/regression/api_spec.rb
@@ -17,8 +17,37 @@ Spec::Matchers.define :equal_regression_fixture do |fixture_name|
   failure_message_for_should { @diff }
 end
 
+###############################################################################
+#
+# These regression files are copied from the manuals. They are only modified to
+# * have the same order as the generated XML
+# * use the same namespace prefixes consistently
+#
+
 describe Adyen::API, "concerning regression examples from the manuals" do
-  it "creates an equal `authorise recurring payment' request body" do
+  it "creates a correct `authorise payment' request body" do
+    @payment = Adyen::API::PaymentService.new({
+      :merchant_account => 'YourMerchant',
+      :reference => 'Your Reference Here',
+      :amount => { :currency => 'EUR', :value => 2000 },
+      :card => {
+        :holder_name => 'Adyen Test',
+        :number => '4111111111111111',
+        :cvc => '737',
+        :expiry_month => '12',
+        :expiry_year => '2012'
+      },
+      :shopper => {
+        :reference => 'Simon Hopper',
+        :email => 's.hopper@test.com',
+        :ip => '61.294.12.12'
+      }
+    })
+    body = @payment.send(:authorise_payment_request_body)
+    body.should equal_regression_fixture(:authorise_payment)
+  end
+
+  it "creates a correct `authorise recurring payment' request body" do
     @payment = Adyen::API::PaymentService.new({
       :merchant_account => 'YourMerchantAccount',
       :reference => 'RecurringPayment-0001',
