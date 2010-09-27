cd15cf816909dad3bd846f7472c487c8a98991d6
Eloy Duran <eloy.de.enige@gmail.com>
2010-09-27 12:43:22 +0200

    Make regression spec work.
    It’s lame though, as the best way to do it ended up being making sure
    the actual XML is formatted as we generate it and in the correct order.
    But the diffing does help.

--- spec/regression/api_spec.rb
+++ spec/regression/api_spec.rb
@@ -1,34 +1,20 @@
 require File.expand_path("../../spec_helper", __FILE__)
 require 'tempfile'
 
-module APISpecHelper
-  def self.xmllint_format(file)
-    `xmllint --format --nsclean '#{file}'`
-  end
-end
-
 Spec::Matchers.define :equal_regression_fixture do |fixture_name|
   match do |actual|
+    equal = false
     regression_file = File.expand_path("../api/#{fixture_name}.xml", __FILE__)
     Tempfile.open("adyen-api-regression-#{fixture_name}") do |actual_file|
       actual_file << actual; actual_file.flush
-      formatted = APISpecHelper.xmllint_format(actual_file.path)
-      actual_file.rewind; actual_file << formatted; actual_file.flush
-      puts File.read(actual_file.path)
-
-      if File.read(actual_file.path) == File.read(regression_file)
-        true
-      else
-        @diff = `diff '#{regression_file}' '#{actual_file.path}'`
-        false
+      unless equal = File.read(actual_file.path) == File.read(regression_file)
+        @diff = `diff -U 1 '#{regression_file}' '#{actual_file.path}'`
       end
     end
+    equal
   end
 
-  failure_message_for_should do |actual|
-    "expected that #{actual} would be a precise multiple of #{expected}"
-    "diff:\n#{@diff}"
-  end
+  failure_message_for_should { @diff }
 end
 
 describe Adyen::API, "concerning regression examples from the manuals" do
