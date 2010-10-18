df60b52eb973a8225e8e64e897be53817295d3bf
Willem van Bergen <willem@vanbergen.org>
2010-10-18 18:22:50 +0200

    Upgrade to RSpec 2.

--- adyen.gemspec
+++ adyen.gemspec
@@ -16,7 +16,7 @@ Gem::Specification.new do |s|
   s.homepage = 'http://github.com/wvanbergen/adyen/wiki'
 
   s.add_development_dependency('rake')
-  s.add_development_dependency('rspec', '>= 1.1.4')
+  s.add_development_dependency('rspec', '~> 2.0')
   
   # Drop or make runtime dependency.
   s.add_development_dependency('activerecord')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,11 @@
-require 'spec_helper'
-
 require 'rubygems'
-require 'spec'
-require 'spec/autorun'
-
-gem 'activesupport', '2.3.9'
-gem 'activerecord', '2.3.9'
-gem 'actionpack', '2.3.9'
-require 'active_support'
+require 'rspec'
 
 $:.unshift File.expand_path('../../lib', __FILE__)
 
 require 'adyen'
 require 'adyen/matchers'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
   config.include Adyen::Matchers
 end
