3be32698692d393943ef3ad321fb173608f1e873
Willem van Bergen <willem@vanbergen.org>
2011-01-21 09:35:05 -0500

    Removed PS3 file

--- t.rb
+++
@@ -1,8 +0,0 @@
-framework File.expand_path("PS3Controller.framework")
-
-@controller = PS3SixAxis.sixAixisControllerWithDelegate self
-
-def connect
-  puts "Connecting to the PS3 controller"
-	@controller.connect(true)
-end
