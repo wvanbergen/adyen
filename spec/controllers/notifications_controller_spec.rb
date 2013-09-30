2c7ba09dda04d88c883c73269a9dbad1c04d7477
njpearman <n.pearman@gmail.com>
2013-09-30 22:30:55 +0200

    moved a gem dependency and added a stub spec

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -49,6 +49,8 @@ describe Adyen::NotificationsController, 'when the wrong credentials are provide
   it 'will not be authorised' do response.response_code.should == 401 end
 end
 
+describe Adyen::NotificationsController, 'when a successful notification is sent twice for the same transaction'
+
 describe Adyen::NotificationsController, 'when no config block is provided to the engine' do
   it 'will raise the expected error' do expect { Adyen.setup }.to raise_error Adyen::ConfigMissing end
 end
