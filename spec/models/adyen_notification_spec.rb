9f54903afc2126b98aee9fd9c891e973e95eee0b
njpearman <n.pearman@gmail.com>
2013-10-02 11:31:20 +0200

    more tightening of notifications model

--- spec/models/adyen_notification_spec.rb
+++ spec/models/adyen_notification_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 module NotificationTestHelper
   def fake_notification
     {
-        psp_reference: generate(:psp_reference),
         event_code: 'AUTHORISATION',
+        event_date: @event_date,
         merchant_reference: 'transaction_code',
         merchant_account_code: 'test_account',
-        event_date: @event_date,
+        psp_reference: generate(:psp_reference),
         success: true
     }
   end
@@ -35,13 +35,6 @@ describe AdyenNotification, 'when logging a valid notification' do
   it 'will save the event date' do @notification.event_date.should == @event_date end
 end
 
-describe AdyenNotification, 'when the merchant reference is missing' do
-  include NotificationTestHelper
-  it 'will raise the expected error' do
-    expect {AdyenNotification.log(notification_without(:merchant_reference))}.to raise_error ActiveRecord::RecordInvalid
-  end
-end
-
 describe AdyenNotification, 'when the event code is missing' do
   include NotificationTestHelper
   it 'will raise the expected error' do
@@ -49,10 +42,10 @@ describe AdyenNotification, 'when the event code is missing' do
   end
 end
 
-describe AdyenNotification, 'when the psp reference is missing' do
+describe AdyenNotification, 'when the event date is missing' do
   include NotificationTestHelper
   it 'will raise the expected error' do
-    expect {AdyenNotification.log(notification_without(:psp_reference))}.to raise_error ActiveRecord::RecordInvalid
+    expect {AdyenNotification.log(notification_without(:event_date))}.to raise_error ActiveRecord::RecordInvalid
   end
 end
 
@@ -60,6 +53,20 @@ describe AdyenNotification, 'when the merchant account code is missing' do
   include NotificationTestHelper
   it 'will raise the expected error' do
     expect {AdyenNotification.log(notification_without(:merchant_account_code))}.to raise_error ActiveRecord::RecordInvalid
+  end
+end
+
+describe AdyenNotification, 'when the merchant reference is missing' do
+  include NotificationTestHelper
+  it 'will raise the expected error' do
+    expect {AdyenNotification.log(notification_without(:merchant_reference))}.to raise_error ActiveRecord::RecordInvalid
+  end
+end
+
+describe AdyenNotification, 'when the psp reference is missing' do
+  include NotificationTestHelper
+  it 'will raise the expected error' do
+    expect {AdyenNotification.log(notification_without(:psp_reference))}.to raise_error ActiveRecord::RecordInvalid
   end
 end
 
