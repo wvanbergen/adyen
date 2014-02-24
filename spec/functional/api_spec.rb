622120dba69b3851c361045699bd8d910e16bffa
Lucas Húngaro <lucashungaro@gmail.com>
2014-02-24 10:38:54 -0300

    Basic installments support (still only on PaymentService, not accessible through Adyen::API wrapper)

--- spec/functional/api_spec.rb
+++ spec/functional/api_spec.rb
@@ -45,7 +45,7 @@ if File.exist?(API_SPEC_INITIALIZER)
     end
 
     it "performs a one-click payment request" do
-      detail   = Adyen::API.list_recurring_details(@user_id).references.last
+      detail   = Adyen::API.list_recurring_details(@user_id).references.last || 'recurring-reference'
       response = Adyen::API.authorise_one_click_payment(
         @order_id,
         { :currency => 'EUR', :value => '1234' },
