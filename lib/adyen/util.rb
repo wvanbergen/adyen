d25ed61790f6b66933cc9b6e911af4cbdf41bcde
Willem van Bergen <willem@vanbergen.org>
2014-09-22 10:04:41 -0400

    Add underscore and deflatten to Adyen::Util.

--- lib/adyen/util.rb
+++ lib/adyen/util.rb
@@ -56,14 +56,17 @@ module Adyen
     # @param [:to_s] identifier The identifier to turn to camelcase.
     # @return [String] The camelcase version of the identifier provided.
     def camelize(identifier)
-      identifier.to_s.gsub(/_(.)/) { $1.upcase }
+      identifier.to_s.gsub(/_+(.)/) { $1.upcase }
     end
 
     # Returns the underscore version of a string.
     # @param [:to_s] identifier The identifier to turn to underscore notation.
     # @return [String] The underscore version of the identifier provided.
     def underscore(identifier)
-      identifier.to_s.gsub(/[A-Z]\w*/)
+      identifier.to_s
+        .gsub(/([A-Z]+)([A-Z])/) { "#{$1.downcase}#{$2}" }
+        .gsub(/(?!\A)([A-Z][a-z]*)/, '_\1')
+        .downcase
     end
 
     # Transforms the nested parameters Hash into a 'flat' Hash which is understood by adyen. This is:
@@ -77,13 +80,13 @@ module Adyen
     #    # resolves in:
     #    {'billingAddress.street' =>  'My Street'}
     #
-    # @param [Hash] parameters The payment parameters which to transform
+    # @param [Hash] nested_hash The nested hash to transform
     # @param [String] prefix The prefix to add to the key
     # @param [Hash] return_hash The new hash which is retruned (needed for recursive calls)
-    # @return [Hash] The return_hash filled with camelized and prefixed key, stringified value
-    def flatten(parameters, prefix = "", return_hash = {})
-      parameters ||= {}
-      parameters.inject(return_hash) do |hash, (key, value)|
+    # @return [Hash] The return hash filled with camelized and prefixed key, stringified value
+    def flatten(nested_hash, prefix = "", return_hash = {})
+      nested_hash ||= {}
+      nested_hash.inject(return_hash) do |hash, (key, value)|
         key = "#{prefix}#{camelize(key)}"
         if value.is_a?(Hash)
           flatten(value, "#{key}.", return_hash)
@@ -92,6 +95,43 @@ module Adyen
         end
         hash
       end
-    end    
+    end
+
+    # Transforms a flat hash into a nested hash structure.
+    #  * all keys are underscored
+    #  * all keys are stringified
+    #  * flattened hash is deflattened, using . as namespace separator
+    #
+    # @example
+    #    deflatten {'billingAddress.street' =>  'My Street'}
+    #
+    #    # resolves in:
+    #    {'billing_address' => { 'street' => 'My Street'}}
+    #
+    # @param [Hash] flattened_hash The flat hash to transform
+    # @param [Hash] return_hash The new hash which will be returned (needed for recursive calls)
+    # @return [Hash] A nested hash structure, using strings as key.
+    def deflatten(flattened_hash, return_hash = {})
+      return return_hash if flattened_hash.nil?
+      flattened_hash.each do |key, value|
+        deflatten_pair(key, value, return_hash)
+      end
+      return_hash
+    end
+
+    private
+
+    def deflatten_pair(key, value, return_hash)
+      head, rest = key.split('.', 2)
+      key = underscore(head)
+      if rest.nil?
+        raise ArgumentError, "Duplicate key in flattened hash." if return_hash.key?(key)
+        return_hash[key] = value
+      else
+        return_hash[key] ||= {}
+        raise ArgumentError, "Key nesting conflict in flattened hash." unless return_hash[key].is_a?(Hash)
+        deflatten_pair(rest, value, return_hash[key])
+      end
+    end
   end
 end
--- test/util_test.rb
+++ test/util_test.rb
@@ -29,9 +29,47 @@ class UtilTest < Minitest::Test
     assert_raises(ArgumentError) { Adyen::Util.format_timestamp(20090101111111) }
   end
 
+  def test_camelize
+    assert_equal 'helloCruelWorld', Adyen::Util.camelize(:hello_cruel_world)
+    assert_equal 'HelloWorld', Adyen::Util.camelize('_hello__world')
+  end
+
+  def test_underscore
+    assert_equal 'hello_cruel_world', Adyen::Util.underscore('HelloCruelWorld')
+    assert_equal 'rest_api', Adyen::Util.underscore('RESTApi')
+  end
+
   def test_flatten
-    parameters = { 'billingAddress.street' => 'My Street' }
-    assert_equal parameters, Adyen::Util.flatten(:billing_address => { :street => 'My Street'})
+    expected_hash = { 'billingAddress.street' => 'My Street' }
+    assert_equal expected_hash, Adyen::Util.flatten(:billing_address => { :street => 'My Street'})
+
     assert_equal Hash.new, Adyen::Util.flatten(nil)
+    assert_equal Hash.new, Adyen::Util.flatten({})
+  end
+
+  def test_deflatten
+    expected_hash = {
+      "payment_details" => {
+        "billing_address" => {
+          "street" => "Bell Street", 
+          "number" => 123, 
+          "city" => "Ottawa"
+        }, 
+        "result" => "Authorized", 
+        "auth_code" => "A40B8"
+      }
+    }
+
+    assert_equal expected_hash, Adyen::Util.deflatten(
+      'paymentDetails.billingAddress.street' => 'Bell Street',
+      'paymentDetails.billingAddress.number' => 123,
+      'paymentDetails.billingAddress.city'   => 'Ottawa',
+      'paymentDetails.result' => 'Authorized',
+      'paymentDetails.authCode' => 'A40B8',
+    )
+
+    assert_raises(ArgumentError) { Adyen::Util.deflatten('a' => 1, 'a.b' => 2) }
+    assert_equal Hash.new, Adyen::Util.deflatten(nil)
+    assert_equal Hash.new, Adyen::Util.deflatten({})
   end
 end
