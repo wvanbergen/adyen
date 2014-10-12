b4b0ded26bbf3099b027687f0de63ccbfbeff100
Willem van Bergen <willem@vanbergen.org>
2014-10-12 07:08:49 -0400

    Add integration test that uses client side encryption to submit a payment.

--- test/helpers/test_server.rb
+++ test/helpers/test_server.rb
@@ -1,13 +1,12 @@
 require 'sinatra'
 
 class Adyen::TestServer < Sinatra::Base
-  set :views, File.join(File.dirname(__FILE__), 'views')
+  set :views,         File.join(File.dirname(__FILE__), 'views')
+  set :public_folder, File.join(File.dirname(__FILE__), 'public')
 
-  def adyen_client
-    @adyen_client ||= begin
-      setup_api_configuration
-      Adyen::REST.client
-    end
+  def initialize
+    super
+    setup_api_configuration
   end
 
   get '/' do
@@ -19,15 +18,21 @@ class Adyen::TestServer < Sinatra::Base
   end
 
   post '/pay' do
-    response = adyen_client.api_request('Payment.authorise', 
+    response = Adyen::REST.client.api_request('Payment.authorise', 
       payment_request: {
         merchant_account: 'VanBergenORG',
         amount: { currency: 'EUR', value: 1234 },
         reference: 'Test order #1',
-        card: params[:card],
         browser_info: {
           acceptHeader: request['Accept'] || "text/html;q=0.9,*/*",
           userAgent: request.user_agent
+        },
+        additionalData: {
+          card: {
+            encrypted: {
+              json: params['adyen-encrypted-data']
+            }
+          }
         }
       }
     )
@@ -51,92 +56,7 @@ class Adyen::TestServer < Sinatra::Base
     end
   end
 
-  get '/capture/:psp_reference' do
-    @psp_reference = params[:psp_reference]
-    erb :capture
-  end
-
-  post '/capture' do
-    response = adyen_client.api_request('Payment.capture', modification_request: {
-      merchant_account: 'VanBergenORG',
-      original_reference: params[:psp_reference],
-      modification_amount: {
-        currency: 'EUR',
-        value: params[:value]
-      }
-    })
-
-    if response['modification_result']['response'] == '[capture-received]'
-      body "Capture received (PSP reference: #{response['modification_result']['psp_reference']})"
-    else
-      status 400
-      body response['modification_result']['response']
-    end
-  end
-
-  get '/cancel/:psp_reference' do
-    @psp_reference = params[:psp_reference]
-    erb :cancel
-  end
-
-  post '/cancel' do
-    response = adyen_client.api_request('Payment.cancel', modification_request: {
-      merchant_account: 'VanBergenORG',
-      original_reference: params[:psp_reference],
-    })
-
-    if response['modification_result']['response'] == '[cancel-received]'
-      body "Cancel received (PSP reference: #{response['modification_result']['psp_reference']})"
-    else
-      status 400
-      body response['modification_result']['response']
-    end
-  end
-
-  get '/refund/:psp_reference' do
-    @psp_reference = params[:psp_reference]
-    erb :refund
-  end
-
-  post '/refund' do
-    response = adyen_client.api_request('Payment.refund', modification_request: {
-      merchant_account: 'VanBergenORG',
-      original_reference: params[:psp_reference],
-      modification_amount: {
-        currency: 'EUR',
-        value: params[:value]
-      }
-    })
-
-    if response['modification_result']['response'] == '[refund-received]'
-      body "Refund received (PSP reference: #{response['modification_result']['psp_reference']})"
-    else
-      status 400
-      body response['modification_result']['response']
-    end
-  end
-
-  get '/refundorcancel/:psp_reference' do
-    @psp_reference = params[:psp_reference]
-    erb :refundorcancel
-  end
-
-  post '/refundorcancel' do
-    response = adyen_client.api_request('Payment.cancelOrRefund', modification_request: {
-      merchant_account: 'VanBergenORG',
-      original_reference: params[:psp_reference],
-    })
-
-    if response['modification_result']['response'] == '[cancelOrRefund-received]'
-      body "CancelOrRefund received (PSP reference: #{response['modification_result']['psp_reference']})"
-    else
-      status 400
-      body response['modification_result']['response']
-    end
-  end    
-
   post '/3dsecure/return' do
-    p params
-    erb :success
+    raise "still to do"
   end
 end
