578043ebe4e7a0efa8a80deacad82845e41d6bd1
Arnout de Mooij <arnoutdemooij@gmail.com>
2015-11-02 17:22:40 +0100

    method to easily obtain an HPP client with default configuration

--- lib/adyen/hpp.rb
+++ lib/adyen/hpp.rb
@@ -5,6 +5,7 @@ require 'adyen/hpp/response'
 
 module Adyen
   module HPP
+
     # The DOMAIN of the Adyen payment system that still requires the current
     # Adyen enviroment.
     HPP_DOMAIN = "%s.adyen.com"
@@ -12,6 +13,15 @@ module Adyen
     # The URL of the Adyen payment system that still requires the current
     # domain and payment flow to be filled.
     HPP_URL = "https://%s/hpp/%s.shtml"
+
+    # Provides a HPP client that is configured using the values in <tt>Adyen.configuration</tt>.
+    # @return [Adyen::HPP::Client] A configured client instance
+    def self.client
+      Adyen::REST::Client.new(
+        Adyen.configuration.environment,
+        Adyen.configuration.default_skin
+      )
+    end
 
     class Error < Adyen::Error
     end
