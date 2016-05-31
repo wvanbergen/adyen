045af75d607717b0aa420aca782bd6c675d8dc82
Matijs van Zuijlen <matijs@matijs.net>
2016-05-31 16:52:16 +0200

    Simplify code to avoid variable name shadowing

--- lib/adyen/configuration.rb
+++ lib/adyen/configuration.rb
@@ -158,9 +158,7 @@ class Adyen::Configuration
   #
   # @return [Hash, nil] A hash with the skin information, or nil if not found.
   def form_skin_by_code(skin_code)
-    if skin = @form_skins.detect { |(name, skin)| skin[:skin_code] == skin_code }
-      skin.last
-    end
+    @form_skins.values.find { |skin| skin[:skin_code] == skin_code }
   end
 
   # Returns the shared secret belonging to a skin.
